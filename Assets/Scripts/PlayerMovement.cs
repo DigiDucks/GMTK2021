2ee4820f9df3d7066b31f5d2c54fd6d208c99653
Alex-Jalonen <71279927+Alex-Jalonen@users.noreply.github.com>
2021-06-11 22:25:11 -0800

    fleshed out test level
    added the boxes and doors to level 1

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -53,16 +53,15 @@ public class PlayerMovement : MonoBehaviour
         //Set a raycast to check whether or not on the ground
         Vector3 max = _col.bounds.max;
         Vector3 min = _col.bounds.min;
-        Vector2 corner1 = new Vector2(max.x - .01f, min.y - .01f);
-        Vector2 corner2 = new Vector2(min.x + .01f, min.y - .01f);
+        Vector2 corner1 = new Vector2(max.x - .03f, min.y - .01f);
+        Vector2 corner2 = new Vector2(min.x + .03f, min.y - .01f);
         Collider2D[] hit = Physics2D.OverlapAreaAll(corner1, corner2);
 
         if (hit != null)
         {
             foreach(Collider2D col in hit)
             {
-           
-                if (!col.isTrigger && col.gameObject !=gameObject)
+                if (!col.isTrigger && col.gameObject !=gameObject )
                 {
                     Debug.Log(col);
                     grounded = true;
